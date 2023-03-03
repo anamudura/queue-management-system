355f5eeaf708ec8c8ae9ddba803ad1717ef6b792
anamudura <anaandreea1228@yahoo.com>
2023-03-03 21:50:31 +0200

    Project

--- src/main/java/SimulationManager.java
+++ src/main/java/SimulationManager.java
@@ -231,7 +231,7 @@ public class SimulationManager implements Runnable {
             }
             currentTime++;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
